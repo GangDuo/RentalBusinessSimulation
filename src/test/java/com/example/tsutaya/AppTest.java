8af889de20dada0e06bf7cc16b2e9664eb4b75a3
Onoda <5295036+GangDuo@users.noreply.github.com>
2024-01-07 18:13:21 +0900

    正しい長期レンタルのレシート

--- src/test/java/com/example/tsutaya/AppTest.java
+++ src/test/java/com/example/tsutaya/AppTest.java
@@ -2,13 +2,28 @@ package com.example.tsutaya;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
-class AppTest 
-{
-    @Test
-    void shouldAnswerWithTrue()
-    {
-		fail("まだ実装されていません");
-    }
+class AppTest {
+	@Test
+	void shouldAnswerWithTrueForLongTermRental() {
+		List<String> expected = Arrays.asList(
+				"Rental Record for マーチン・ファウラー",
+				"\tsun\t3.0",
+				"\tmoon\t21.0",
+				"\tpolice\t9.5",
+				"Amount owed is 33.5",
+				"You earned 4 frequent renter points");
+
+		Customer c = new Customer("マーチン・ファウラー");
+		c.addRental(new Rental(4, new Movie(Movie.CHILDRENS, "sun")));
+		c.addRental(new Rental(7, new Movie(Movie.NEW_RELEASE, "moon")));
+		c.addRental(new Rental(7, new Movie(Movie.REGULAR, "police")));
+		String ss = c.statement();
+		List<String> actual = Arrays.asList(ss.split("[\r\n]+"));
+		assertLinesMatch(expected, actual);
+	}
 }
